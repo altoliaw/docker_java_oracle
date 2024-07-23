2422c2d5dd7d1e8866fd35fd32153b52bf2fadc5
altoliaw <altoliaw@hotmail.com>
2024-07-23 18:33:39 +0800

    Using PL/SQL prepared statements in JAVA code

--- workspace/Java-Oracle-Project/app/StartedEntryVersion6.java
+++ workspace/Java-Oracle-Project/app/StartedEntryVersion6.java
@@ -1,10 +1,12 @@
 package app;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 
 class StartedEntryVersion6 {
 
@@ -22,6 +24,7 @@ class StartedEntryVersion6 {
       String jdbcUrl = "jdbc:oracle:thin:@" + DB_IP + ":1521:" + DB_SID;
       int rowNum = 971 / 3;
       Connection connection = null;
+      CallableStatement callableStatement = null;
       PreparedStatement pstmt = null;
       ResultSet rs = null;
 
@@ -29,22 +32,28 @@ class StartedEntryVersion6 {
          Class.forName("oracle.jdbc.driver.OracleDriver");
 
          connection = DriverManager.getConnection(jdbcUrl, DB_USERNAME, DB_PASSWORD);
-         System.out.println("Connected to Oracle database successfully.");
+         String plsql1 = "BEGIN OPEN :stmt FOR SELECT * FROM SYSTEM.HELP WHERE ROWNUM <= ?; END;";
+         callableStatement = connection.prepareCall(plsql1);
+         callableStatement.registerOutParameter(1, oracle.jdbc.OracleTypes.CURSOR);
+         callableStatement.setInt(2, 100);
+         callableStatement.execute();
 
-         // String sql = "SELECT * FROM (SELECT t.* FROM SYSTEM.HELP t) WHERE ROWNUM <= ?";
-         String sql = "SELECT t.* FROM SYSTEM.HELP t";
-         pstmt = connection.prepareStatement(sql);
 
-         for (int i = 1, j = 1; i <= 1; i++) {
-            // pstmt.setInt(1, rowNum * i);
-            // pstmt.setInt(1, j++);
+         // System.out.println("Connected to Oracle database successfully.");
+         // // String sql = "SELECT * FROM (SELECT t.* FROM SYSTEM.HELP t) WHERE ROWNUM <= ?";
+         // String sql = "SELECT t.* FROM SYSTEM.HELP t";
+         // pstmt = connection.prepareStatement(sql);
 
-            rs = pstmt.executeQuery();
-            while (rs.next()) {
-               System.out.println("Topic: " + rs.getString("topic") + ", Sequence: " + rs.getInt("seq") + ", Info: " + rs.getString("info"));
-            }
-            rs.close();
-         }
+         // for (int i = 1, j = 1; i <= 1; i++) {
+         //    // pstmt.setInt(1, rowNum * i);
+         //    // pstmt.setInt(1, j++);
+
+         //    rs = pstmt.executeQuery();
+         //    while (rs.next()) {
+         //       System.out.println("Topic: " + rs.getString("topic") + ", Sequence: " + rs.getInt("seq") + ", Info: " + rs.getString("info"));
+         //    }
+         //    rs.close();
+         // }
       } catch (ClassNotFoundException e) {
          System.err.println("Oracle JDBC Driver not found. Include it in your library path.");
          e.printStackTrace();
